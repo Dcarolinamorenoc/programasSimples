8aa29633a4a34c64b64a650838d3a4deda565624
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-19 12:25:09 -0500

    Ejercicio Círculos

---
+++ 2.circulos.py
@@ -0,0 +1,9 @@
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+
+radio=float(input("Escribe el radio del círculo: "))
+
+perimetro=2*3.1416*radio
+area=3.1416*radio**2
+
+print("El perímetro del círculo es: ",perimetro )
+print("El area del círculo es: ", area )
