23e772576ddad748a2813351c043c15ab014b3c3
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-19 12:25:28 -0500

    Ejercicio Promedio

---
+++ 3.promedio.py
@@ -0,0 +1,10 @@
+#Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
+
+not1=float(input("Ingrese la primera nota: "))
+not2=float(input("Ingrese la segunda nota: "))
+not3=float(input("Ingrese la tercera nota: "))
+not4=float(input("Ingrese la cuarta nota: "))
+
+promedio=(not1+not2+not3+not4)/4
+
+print("Querido usuario el promedio de sus notas es", promedio)
