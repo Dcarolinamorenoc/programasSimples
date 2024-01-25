90136ba18b8602caaf000e3334da972f537cb7e7
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-01-25 18:08:50 -0500

    Mensaje importante sobre la realizacion del trabajo

---
+++ 1.mensajeSobreLosEjercicios.py
@@ -0,0 +1,3 @@
+#Hola Profesor, le queria comentar que si los archivos salen que los trabaje en corto tiempo fue porque yo los subi al drive y como en clase aprendimos a subir los
+#trabajos a github lo que hice fue descargar los archivos y pegarlos en la carpeta que cree en clase (me paso con este trabajo de programas simples y con el de estructuras condicionales)
+# espero me entienda, gracias, buen día :)
